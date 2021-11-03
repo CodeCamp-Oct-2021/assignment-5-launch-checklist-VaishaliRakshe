bbd919bb9f8532fe44cdcd8d73b7cccfed8453cb
VaishaliRakshe <vaishalijadhav000@gmail.com>
2021-11-03 14:49:04 -0500

    update

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ window.addEventListener("load", function() {
 
    let listedPlanets;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
+   myFetch("").then(function(result){
+          
    let listedPlanetsResponse;
    listedPlanetsResponse.then(function (result) {
        listedPlanets = result;
@@ -12,5 +14,6 @@ window.addEventListener("load", function() {
        console.log(listedPlanets);
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
    })
+})
    
 });
